d491fb55d8b7a4826778c202f4cd1202872a5676
agusti544 <31078984+agusti544@users.noreply.github.com>
2018-10-06 02:12:09 +0700

    Update tokopedia.sh

--- tokopedia.sh
+++ tokopedia.sh
@@ -28,7 +28,7 @@ fi
 ngelog(){
 cok="_gorilla_csrf=MTUzODc2NTE5MXxJa05MYmxGeFpsVlFMM0ZuUjFrNVZtVTNlbmRGZDNodFRHUkJVV1ZqZWxSV2NrWkNhVEZuU0dJd2VrRTlJZ289fNRwu_AgdMr0r4de6a4q4zqVuJglzy6yIcGoYj1OnUnS; _ga=GA1.2.68010496.1538498398; _gcl_au=1.1.111113656.1538498398; __auc=de8ce90216635a68829b4678ff7; scs=%7B%22t%22%3A1%7D; _BID_TOKOPEDIA_=1454fff044ad2b3decd929eff9c932c4; lang=id; insdrSV=2; __asc=21113cd3166458b866d8a351bbc; pulsa-aff=undefined; _gid=GA1.2.177169813.1538765065; zarget_visitor_info=%7B%7D; USER_DATA=%7B%22attributes%22%3A%5B%5D%2C%22subscribedToOldSdk%22%3Afalse%2C%22deviceUuid%22%3A%2212239bd7-3cd6-4cf7-b6af-7caa8d4618d3%22%2C%22deviceAdded%22%3Afalse%7D; SOFT_ASK_STATUS=%7B%22actualValue%22%3A%22not%20shown%22%2C%22MOE_DATA_TYPE%22%3A%22string%22%7D; HARD_ASK_STATUS=%7B%22actualValue%22%3A%22prompt%22%2C%22MOE_DATA_TYPE%22%3A%22string%22%7D; o_Pulsa=eyJsYXN0X2RlY2xpbmUiOiIiLCJyZWZlcnJlciI6Imh0dHBzOi8vcHVsc2EudG9rb3BlZGlhLmNvbS9naWZ0LWNhcmQvdG9rb3BlZGlhL3JlZGVlbS8iLCJpc19taWNyb3NpdGUiOmZhbHNlLCJ1dWlkIjoiMjRkMThiNWItNGZmYy00NjAzLWIwMmEtZGE4OTM0MmVlMzJhIiwiaXNfYWpheCI6ZmFsc2V9; _SID_Tokopedia_=E3C3-n6EEwjrJWf8aU14n1ubxk9iAy-JHKIsWpJ4Xw92hifxjjBk__-lWvZTYqM9b_iGgBgp7DqWkZP54rNxol2FIcyK0KiC49Ijeo4uJVzIHGkh2gdjjtZyZu3A4Xsf; l=1; _ga=GA1.3.68010496.1538498398; _gid=GA1.3.177169813.1538765065; OPT_IN_SHOWN_TIME=1538765852885"
 	curl -s -X GET \
---url "https://pulsa.tokopedia.com/gift-card/tokopedia/redeem/check?voucher_code=FPL$1" \
+--url "https://pulsa.tokopedia.com/gift-card/tokopedia/redeem/check?voucher_code=443258" \
 -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
 -H "accept-encoding: gzip, deflate, br" \
 -H "cookie: $cok" \
